e3c62a5a7ab9f347b737f63b8b951b241ccad577
Jayden11git <jaydenskuse54@gmail.com>
2025-07-22 18:30:57 +1000

    read from database

--- UserInformation.py
+++ UserInformation.py
@@ -1,4 +1,4 @@
-from nicegui import ui
+from nicegui import ui, app
 import elements
 import SessionHistory
 
@@ -7,6 +7,8 @@ def header():
     
 @ui.page('/userInformation')
 def main():
+    patient = app.storage.user.get('patient')
+    print(patient.month_year_birth)
     ui.page_title("SocketFit Dashboard")
     header()
     with ui.row().classes('w-full'):
@@ -20,15 +22,19 @@ def main():
                 with ui.row().classes('w-2/5 items-start'):
                     ui.input(placeholder='Users First Name', label='First Name').classes('w-full border rounded-md border-[#3545FF]')
                     ui.input(placeholder='Users Last Name', label='Last Name').classes('w-full border rounded-md border-[#3545FF]')
-                    ui.input(placeholder='Users Date of Birth', label='Date of Birth').classes('w-full border rounded-md border-[#3545FF]')
-                    ui.input(placeholder='Users Weight', label='Weight (kg)').classes('w-full border rounded-md border-[#3545FF]')
-                    ui.input(placeholder='Users Gender', label='Gender').classes('w-full border rounded-md border-[#3545FF]')
-                    ui.input(placeholder='Users Height', label='Height (cm)').classes('w-full border rounded-md border-[#3545FF]')
-                    ui.input(placeholder='Users Amputation Type', label='Amputation Type').classes('w-full border rounded-md border-[#3545FF]')
+                    ui.input(placeholder='Users Date of Birth', label='Date of Birth', value = patient.month_year_birth).classes('w-full border rounded-md border-[#3545FF]')
+                    ui.input(placeholder='Users Weight', label='Weight (kg)', value=patient.weight).classes('w-full border rounded-md border-[#3545FF]')
+                    ui.input(placeholder='Users Gender', label='Gender', value=patient.gender).classes('w-full border rounded-md border-[#3545FF]')
+                    ui.input(placeholder='Users Height', label='Height (cm)', value=patient.height).classes('w-full border rounded-md border-[#3545FF]')
+                    ui.input(placeholder='Users Amputation Type', label='Amputation Type', value=patient.amputation_type).classes('w-full border rounded-md border-[#3545FF]')
                 ui.space()
                 with ui.column().classes('w-2/5'):
                     ui.label("Additional Notes").classes('text-xl self-start')
                     ui.textarea(placeholder='Enter Notes Here').classes('h-full w-full border rounded-md border-[#3545FF]')
     ui.button("Next", on_click=SessionHistory.navigateSession)
 def navigate():
+    ui.navigate.to('/userInformation')
+
+def navigatePatient(patient):
+    app.storage.user['patient'] = patient
     ui.navigate.to('/userInformation')
