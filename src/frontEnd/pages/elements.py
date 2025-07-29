4c80d97677ca78213bbd0dd23d9ccbd945a4e8c3
Tahir Kamal <109749722+GoodGuycoding@users.noreply.github.com>
2025-07-29 18:11:18 +1000

    just pushin to fetch latest changes

--- src/frontEnd/pages/elements.py
+++ src/frontEnd/pages/elements.py
@@ -3,4 +3,3 @@ from nicegui import ui
 def header():
     with ui.header().style('background-color: #FFFFFF'):
         ui.image('src\\frontEnd\\assets\\SocketFitDashboard.png').classes('h-[40px] w-[140px]')
-
