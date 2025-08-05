9f5ff014dc8252ca7d291a84c012a8b72e10a0f6
Jayden11git <jaydenskuse54@gmail.com>
2025-08-05 13:44:47 +1000

    Update UserInformation.py

--- src/frontEnd/pages/UserInformation.py
+++ src/frontEnd/pages/UserInformation.py
@@ -22,13 +22,20 @@ def main():
         with ui.card().classes('w-3/4 h-full border border-[#2C25B2]') as main:
             with ui.row():
                 with ui.row().classes('w-2/5 items-start'):
-                    ui.input(placeholder='Users First Name', label='First Name').classes('w-full border rounded-md border-[#3545FF]')
-                    ui.input(placeholder='Users Last Name', label='Last Name').classes('w-full border rounded-md border-[#3545FF]')
-                    ui.input(placeholder='Users Date of Birth', label='Date of Birth', value = patient.month_year_birth).classes('w-full border rounded-md border-[#3545FF]')
-                    ui.input(placeholder='Users Weight', label='Weight (kg)', value=patient.weight).classes('w-full border rounded-md border-[#3545FF]')
-                    ui.input(placeholder='Users Gender', label='Gender', value=patient.gender).classes('w-full border rounded-md border-[#3545FF]')
-                    ui.input(placeholder='Users Height', label='Height (cm)', value=patient.height).classes('w-full border rounded-md border-[#3545FF]')
-                    ui.input(placeholder='Users Amputation Type', label='Amputation Type', value=patient.amputation_type).classes('w-full border rounded-md border-[#3545FF]')
+                    with ui.input(placeholder='Users First Name', label='First Name').classes('w-full h-full border rounded-md border-[#3545FF]'):
+                        ui.icon('o_edit').classes('text-black text-3xl h-full flex items-center mr-2')
+                    with ui.input(placeholder='Users Last Name', label='Last Name').classes('w-full border rounded-md border-[#3545FF]'):
+                        ui.icon('o_edit').classes('text-black text-3xl h-full flex items-center mr-2')
+                    with ui.input(placeholder='Users Date of Birth', label='Date of Birth', value = patient.month_year_birth).classes('w-full border rounded-md border-[#3545FF]'):
+                        ui.icon('o_calendar_month').classes('text-black text-3xl h-full flex items-center mr-2')
+                    with ui.input(placeholder='Users Weight', label='Weight (kg)', value=patient.weight).classes('w-full border rounded-md border-[#3545FF]'):
+                        ui.icon('o_edit').classes('text-black text-3xl h-full flex items-center mr-2')
+                    with ui.input(placeholder='Users Gender', label='Gender', value=patient.gender).classes('w-full border rounded-md border-[#3545FF]'):
+                        ui.icon('o_edit').classes('text-black text-3xl h-full flex items-center mr-2')
+                    with ui.input(placeholder='Users Height', label='Height (cm)', value=patient.height).classes('w-full border rounded-md border-[#3545FF]'):
+                        ui.icon('o_edit').classes('text-black text-3xl h-full flex items-center mr-2')
+                    with ui.input(placeholder='Users Amputation Type', label='Amputation Type', value=patient.amputation_type).classes('w-full border rounded-md border-[#3545FF]'):
+                        ui.icon('o_edit').classes('text-black text-3xl h-full flex items-center mr-2')
                 ui.space()
                 with ui.column().classes('w-2/5'):
                     ui.label("Additional Notes").classes('text-xl self-start')
